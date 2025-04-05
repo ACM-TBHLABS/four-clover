6dcb90dffbfdae2beaf57484b4426c7096dcf0f7
LynnT-2003 <lnyinyi22@gmail.com>
2025-04-05 20:33:09 +0700

    Deployment error.

--- types/event.ts
+++ types/event.ts
@@ -1,23 +1,23 @@
-import { Image } from "./image";
+import { Image } from "./Image";
 
 export interface Event {
-	_id: string;
-	tag: string;
-	name: string;
-	slug: { current: string };
-	description: string;
-	tagline?: string;
-	start_date: string; // ISO 8601 format (datetime)
-	end_date: string;
-	location: string;
-	country: string;
-	intro_title: string;
-	intro_description?: string;
-	intro_video_url?: string;
-	intro_image: Image;
-	display_on_main_screen?: boolean;
-	content?: Array<
-		| { _type: "block"; children: any[] } // Blocks can contain various rich text elements
-		| { _type: "image"; asset: Image; alt?: string }
-	>;
+  _id: string;
+  tag: string;
+  name: string;
+  slug: { current: string };
+  description: string;
+  tagline?: string;
+  start_date: string; // ISO 8601 format (datetime)
+  end_date: string;
+  location: string;
+  country: string;
+  intro_title: string;
+  intro_description?: string;
+  intro_video_url?: string;
+  intro_image: Image;
+  display_on_main_screen?: boolean;
+  content?: Array<
+    | { _type: "block"; children: any[] } // Blocks can contain various rich text elements
+    | { _type: "image"; asset: Image; alt?: string }
+  >;
 }
