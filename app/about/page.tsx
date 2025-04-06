1fc811595b9d182b2ec8d65373f795969ec59003
saipepu <saipepu.mdy257@gmail.com>
2025-04-06 16:49:48 +0700

    feat: create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,29 @@
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/sections/about/HeroSection";
+import IntroSection from "@/components/sections/about/IntroSection";
+import LeadershipSection from "@/components/sections/about/LeadershipSection";
+import OurJourney from "@/components/sections/about/OurJourney";
+import VisionMissionSection from "@/components/sections/about/VisionMissionSection";
+import FAQSection from "@/components/sections/landing/FAQSection";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full flex flex-col items-center overflow-x-hidden">
+      <HeroSection />
+
+      <div className="max-w-screen md:max-w-[1600px] w-full">
+        <div className="px-[24px] md:px-[150px]">
+          <IntroSection />
+          <VisionMissionSection />
+          <OurJourney />
+          <LeadershipSection />
+        </div>
+        <FAQSection />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default page;
