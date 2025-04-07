5831565d2db8644a532566eb738461e7501f9525
LynnT-2003 <lnyinyi22@gmail.com>
2025-04-07 14:56:04 +0700

    Fix: Deployment Error #5

--- app/events/[slug]/page.jsx
+++ app/events/[slug]/page.jsx
@@ -1,12 +1,8 @@
-// app/events/[slug]/page.tsx
+// app/events/[slug]/page.jsx
 import EventDetailSection from "./EventDetailSection";
 
-export default async function EventPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const { slug } = await params;
+export default async function EventPage({ params }) {
+  const { slug } = params;
 
   return (
     <div className="w-full flex items-start justify-center">
