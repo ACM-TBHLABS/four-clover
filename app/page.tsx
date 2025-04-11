e4916c009424d5fa58815c3a8e0a048c4840d79b
saipepu <saipepu.mdy257@gmail.com>
2025-04-12 03:01:09 +0700

    Adjust layout and component order on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,17 @@ import FAQSection from "@/components/sections/landing/FAQSection";
 import HeroSection from "@/components/sections/landing/HeroSection";
 import IntroSection from "@/components/sections/landing/IntroSection";
 import PartnersSection from "@/components/sections/landing/PartnersSection";
-import SolutionsSection from "@/components/sections/landing/SolutionsSection";
 import SpotlightSection from "@/components/sections/landing/SpotlightSection";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col items-center overflow-x-hidden">
       <HeroSection />
-      <div className="max-w-screen md:max-w-[1600px] w-full ">
+      <div className="max-w-screen md:max-w-[1400px] w-full ">
         <IntroSection />
-        {/* <SolutionsSection /> */}
         <SpotlightSection />
-        <FAQSection />
         <PartnersSection />
+        <FAQSection />
       </div>
     </div>
   );
