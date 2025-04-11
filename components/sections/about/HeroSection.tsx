081f2504f99124dbc5acf02d6f982e84fb5eddf9
saipepu <saipepu.mdy257@gmail.com>
2025-04-12 03:03:21 +0700

    Adjust max height and width of HeroSection for improved responsiveness

--- components/sections/about/HeroSection.tsx
+++ components/sections/about/HeroSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="h-[400px] md:h-[800px] w-full relative flex flex-col justify-end items-start pb-2 md:pb-12">
+    <div className="relative h-[250px] md:h-[400px] lg:h-[600px] w-full flex flex-col justify-end items-start pb-2 md:pb-12">
       {/* Full-screen blur background */}
       <div className="w-full h-full absolute top-0 left-0 z-10 bg-gradient-to-b from-green-900/5 to-green-900/10"></div>
       <img
@@ -10,7 +10,7 @@ const HeroSection = () => {
         alt="products"
         className="w-full h-full max-h-[800px] object-cover absolute top-0 left-0 z-0"
       />
-      <h1 className="w-full max-w-[1600px] mx-auto text-white font-helvetica font-normal text-[40px] md:text-[100px] md:leading-[114.99px] z-20  px-[20px] md:px-[150px]">
+      <h1 className="w-full max-w-[1400px] mx-auto text-white font-helvetica font-normal text-[32px] md:text-[56px] lg:text-[72px] z-20 px-[24px] md:px-[50px] lg:px-[150px] leading-none">
         About Company
       </h1>
     </div>
