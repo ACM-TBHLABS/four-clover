227f4614edba41229fec3cbeb0027af96e5296a1
saipepu <saipepu.mdy257@gmail.com>
2025-04-12 04:38:38 +0700

    Refactor import path for FAQSection and adjust layout for improved responsiveness

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import HeroSection from "@/components/sections/products/HeroSection";
 import ProductsSection from "@/components/sections/products/ProductsSection";
 import CtaSection from "@/components/sections/products/CtaSection";
-import FAQSection from "@/components/sections/landing/FAQSection";
+import FAQSection from "@/components/FAQSection";
 
 const ProductsPage = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center overflow-x-hidden">
       <div className="max-w-screen md:max-w-[1400px] w-full">
-        <div className="px-[24px] md:px-[150px]">
+        <div className="flex flex-col justify-start items-start gap-8 md:gap-[50px] lg:gap-[100px] px-6 md:px-[50px] lg:px-[150px]">
           <HeroSection />
           <ProductsSection />
           <CtaSection />
