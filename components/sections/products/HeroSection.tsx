065590dbf05023c4796a9e1fd81dc5788d9f8806
saipepu <saipepu.mdy257@gmail.com>
2025-04-12 04:39:39 +0700

    Refactor HeroSection layout for improved typography and spacing

--- components/sections/products/HeroSection.tsx
+++ components/sections/products/HeroSection.tsx
@@ -2,14 +2,12 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="py-[100px] flex flex-col gap-[32px]">
-      <h1 className="font-helvetica font-normal text-[32px] md:text-[72px] md:leading-[82.79px]">
+    <div className="flex flex-col gap-2 pt-[100px]">
+      <h1 className="font-helvetica font-normal text-[32px] md:text-[56px] lg:text-[72px] md:leading-[82.79px]">
         Product List
       </h1>
-      <h1 className="font-helvetica font-light text-[16px] md:text-[32px] md:leading-[36.8px]">
-        Explore our comprehensive range of cutting-edge dental solutions,
-        designed to enhance precision, efficiency, and patient care in every
-        clinic and hospital
+      <h1 className="font-helvetica font-light text-[18px] md:text-[24px] lg:text-[32px] leading-[1.1]">
+        Explore our comprehensive range of cutting-edge dental solutions, designed to enhance precision, efficiency, and patient care in every clinic and hospital
       </h1>
     </div>
   );
