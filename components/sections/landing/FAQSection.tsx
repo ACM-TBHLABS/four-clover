f3c92d9040f906b17794c45f5c45df9ab308d47b
saipepu <saipepu.mdy257@gmail.com>
2025-04-12 03:03:42 +0700

    Refactor FAQSection layout and improve responsiveness

--- components/sections/landing/FAQSection.tsx
+++ components/sections/landing/FAQSection.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React, { useEffect } from "react";
 import {
   Accordion,
@@ -7,6 +7,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { fetchAllFAQs } from "@/services/api/faqsService";
+import { ArrowUpRight } from "lucide-react";
 
 const FAQSection = () => {
   const [faqs, setFaqs] = React.useState<any[]>([]);
@@ -21,8 +22,7 @@ const FAQSection = () => {
       }
     };
     fetchFAQs();
-  }
-  , []);
+  }, []);
 
   if (!faqs) {
     return (
@@ -33,8 +33,8 @@ const FAQSection = () => {
   }
 
   return (
-    <div className="my-[100px] mx-[24px] md:mx-[150px] flex flex-col gap-2 md:gap-[20px]">
-      <h1 className="font-helvetica font-normal text-[32px] md:text-[72px]">
+    <div className="py-[50px] pb-[100px] px-[24px] md:px-[50px] lg:px-[150px] flex flex-col gap-2 md:gap-[20px]">
+      <h1 className="font-helvetica font-normal text-[32px] md:text-[56px] lg:text-[72px] leading-[1.1]">
         FAQs
       </h1>
       <Accordion type="multiple">
@@ -44,27 +44,43 @@ const FAQSection = () => {
             value={`item-${index + 1}`}
             className="border-b-[1px] border-solid border-[#5a5a5a]"
           >
-            <AccordionTrigger className="py-[20px]">
-              <h1 className="font-helvetica font-normal text-[20px] md:text-[24px] leading-[27.6px]">
-                {index + 1}. {faq.question}
-              </h1>
+            <AccordionTrigger className="py-[20px] flex items-start justify-betweeen">
+              <ul className="flex flex-col gap-[12px]">
+                <li className="flex justify-start items-start gap-1 font-helvetica font-normal text-[18px] md:text-[24px] leading-[1.1]">
+                  <span className="block">{index + 1}.</span>
+                  <span className="block">{faq.question}</span>
+                </li>
+              </ul>
             </AccordionTrigger>
             <AccordionContent>
-              <h1 className="font-helvetica font-normal text-[20px] md:text-[24px] leading-[27.6px] py-[20px] ml-6">
-                {faq.answer}
-              </h1>
-              {/* Optional: if ref_links exists and you want to show them */}
-              {faq.ref_links && faq.ref_links.length > 0 && (
-                <ul className="ml-10  list-disc text-sm underline">
-                  {faq.ref_links?.map((link: string, i: number) => (
-                    <li key={i}>
-                      <a href={link} target="_blank" rel="noopener noreferrer">
-                        {link}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              )}
+              <div className="flex justify-start items-start gap-1 font-helvetica font-normal text-[20px] md:text-[24px] leading-none">
+                <p className="block opacity-0">{index + 1}.</p>
+                <div className="block">
+                  <p className="mb-8">{faq.answer}</p>
+                  <div className="w-full flex flex-col justify-start items-start gap-1">
+                    <p className="font-helvetica font-normal text-[20px] md:text-[24px] leading-[1.1]">
+                      References:
+                    </p>
+                    {faq.ref_links && faq.ref_links.length > 0 && (
+                      <div className="font-light hover:underline group">
+                        {faq.ref_links?.map((link: string, i: number) => (
+                          <a
+                            key={i}
+                            href={link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {link.split("https://www.")[1]}
+                            <span className="inline-block group-hover:-translate-y-[2px] group-hover:translate-x-[2px] transition-transform duration-300 ease-in-out">
+                              <ArrowUpRight className="inline-block ml-1" />
+                            </span>
+                          </a>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
             </AccordionContent>
           </AccordionItem>
         ))}
