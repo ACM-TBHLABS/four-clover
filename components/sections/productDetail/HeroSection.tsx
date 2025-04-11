506c1c254b377a1a987c800bce2ae2c76f268453
saipepu <saipepu.mdy257@gmail.com>
2025-04-12 04:39:18 +0700

    Refactor HeroSection layout for improved responsiveness and visual clarity

--- components/sections/productDetail/HeroSection.tsx
+++ components/sections/productDetail/HeroSection.tsx
@@ -12,7 +12,7 @@ interface Props {
 const HeroSection: React.FC<Props> = ({ image, name }) => {
   const router = useRouter();
   return (
-    <div className="h-[400px] md:h-[800px] w-full relative">
+    <div className="h-[400px] md:h-[800px] w-full relative flex justify-center items-start ">
       {/* Full-screen blur background */}
       <img
         src="/products/blur_bg.png"
@@ -21,20 +21,20 @@ const HeroSection: React.FC<Props> = ({ image, name }) => {
       />
 
       {/* Constrain max width and ensure relative positioning */}
-      <div className="w-full max-w-[1400px] mx-auto h-full relative flex items-center justify-center">
-        <img
-          src={urlFor(image).url()}
-          alt="products"
-          className="w-[874.67px] h-[490.67px] object-contain md:object-cover"
-        />
-        <h1 className="absolute bottom-4 md:bottom-12 left-[24px] md:left-[150px] text-white font-helvetica font-normal text-[32px] md:text-[100px] leading-[114.99px]">
-          {name}
-        </h1>
+      <div className="w-full max-w-[1400px] h-full relative flex flex-col items-start justify-start px-6 md:px-[50px] lg:px-[150px] py-5">
         <ArrowLeft
-          className="absolute top-4 md:top-12 left-[12px] md:left-[150px] text-black w-8 md:w-12 h-8 md:h-12 cursor-pointer"
+          className="text-black w-8 md:w-12 h-8 md:h-12 cursor-pointer"
           strokeWidth={1}
           onClick={() => router.push("/products")}
         />
+        <img
+          src={urlFor(image).url()}
+          alt="products"
+          className="w-full max-h-[600px] object-contain md:object-cover mix-blend-multiply"
+        />
+        <h1 className="text-white font-helvetica font-normal text-[32px] md:text-[56px] lg:text-[72px]">
+          {name}
+        </h1>
       </div>
     </div>
   );
