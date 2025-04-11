6e322cde65da3e8bf4e5d29bf401ed9f082fe998
saipepu <saipepu.mdy257@gmail.com>
2025-04-12 03:03:59 +0700

    Refactor SpotlightSection layout and improve responsiveness

--- components/sections/landing/SpotlightSection.tsx
+++ components/sections/landing/SpotlightSection.tsx
@@ -1,14 +1,10 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Image from "next/image";
 import { fetchAllPartnerships } from "@/services/api/partnershipService";
-import { usePartnerships } from "@/services/query/usePartnerships";
 import { urlFor } from "@/sanity/lib/image";
 import { Partnership } from "@/types/partnership";
 
 const SpotlightSection = () => {
-  // const { data: partnerships, isLoading, error } = usePartnerships();
-
   const [partnerships, setPartnerships] = useState<Partnership[]>([]);
 
   useEffect(() => {
@@ -19,60 +15,25 @@ const SpotlightSection = () => {
     fetchPartnerships();
   }, []);
 
-  // if (isLoading) return <p>Loading partnerships...</p>;
-  // if (error) return <p>Error loading partnerships</p>;
-
   return (
-    <div className="px-[24px] md:px-[150px] py-[100px]">
-      <div className="w-full flex flex-col gap-[100px]">
-        <div className="flex flex-col gap-[32px]">
-          <h1 className="font-helvetica font-normal text-[32px] md:text-[72px] md:leading-[82.79px]">
+    <div className="px-[24px] md:px-[50px] lg:px-[150px] py-[50px] md:py-[50px] lg:py-[100px]">
+      <div className="w-full flex flex-col gap-8 lg:gap-[100px]">
+        <div className="flex flex-col gap-5 md:gap-8">
+          <h1 className="font-helvetica font-normal text-[32px] md:text-[56px] lg:text-[72px] leading-[1.1]">
             Collaborating for Innovation
           </h1>
-          <h1 className="font-helvetica font-light text-[16px] md:text-[32px] w-full md:w-1/2">
+          <h1 className="font-helvetica font-light text-[18px] md:text-[24px] lg:text-[32px] w-full lg:w-1/2">
             Our journey toward advancing dental solutions is powered by leading
             institutions and businesses that share our vision for innovation,
             quality, and excellence.
           </h1>
         </div>
         <div className="w-full flex flex-col gap-[32px]">
-          <h1 className="font-helvetica font-normal text-[32px] md:text-[72px] leading-[82.79px]">
+          <h1 className="font-helvetica font-normal text-[32px] md:text-[56px] lg:text-[72px] leading-[1.1]">
             Spotlight Partnerships
           </h1>
 
-          <div className="flex flex-col gap-[32px]">
-            {partnerships
-              ?.filter(
-                (partnership) =>
-                  partnership.description &&
-                  partnership.description.trim() !== ""
-              )
-              .map((partnership) => (
-                <div className="md:hidden flex flex-col" key={partnership._id}>
-                  <div className="flex flex-col w-full gap-[8px]">
-                    <div className="flex flex-col gap-[8px] items-start">
-                      <img
-                        src={urlFor(partnership.coverPhoto).url()}
-                        alt={partnership.name}
-                        className="w-full h-[300px] object-cover"
-                      />
-                      <img
-                        src={urlFor(partnership.logo).url()}
-                        alt={partnership.name}
-                        className="w-auto h-[80px] object-contain"
-                      />
-                    </div>
-                    <div className="w-full">
-                      <h1 className="font-helvetica font-light text-[18px]">
-                        {partnership.description}
-                      </h1>
-                    </div>
-                  </div>
-                </div>
-              ))}
-          </div>
-
-          <div className="flex-col gap-[32px] hidden md:flex">
+          <div className="flex flex-col justify-start items-start gap-[50px] md:gap-5 lg:gap-8">
             {partnerships
               ?.filter(
                 (partnership) =>
@@ -82,37 +43,41 @@ const SpotlightSection = () => {
               .slice(0, 2)
               .map((partnership) => (
                 <div
-                  className="flex flex-row gap-[32px] justify-between"
+                  className="flex flex-col md:flex-row gap-5 lg:gap-8 justify-between"
                   key={partnership._id}
                 >
-                  <div className="h-[300px] flex flex-col w-1/2">
-                    <div className="mt-auto w-[590px]">
-                      <h1 className="font-helvetica font-normal text-[16px] md:text-[32px]">
+                  <div className="flex flex-col-reverse md:flex-col md:w-1/2 gap-1">
+                    <div className="mt-auto">
+                      <h1 className="hidden md:block font-helvetica font-normal text-[16px] md:text-[24px] lg:text-[32px]">
                         {partnership.name}
                       </h1>
-                      <h1 className="font-helvetica font-light text-[18px]">
+                      <h1 className="font-helvetica font-light text-[16px] lg:text-[18px] leading-[1.1]">
                         {partnership.description}
                       </h1>
                     </div>
+                    {/* this one is just to preserve the bottom space */}
+                    <img
+                      src={urlFor(partnership.logo).url()}
+                      alt={partnership.name}
+                      className="w-fit h-[40px] lg:h-[80px] object-contain md:opacity-0"
+                    />
                   </div>
-                  <div className="flex flex-col gap-[8px] w-1/2 justify-end items-end">
-                    <div className="flex flex-col gap-[8px]">
-                      <img
-                        src={urlFor(partnership.coverPhoto).url()}
-                        alt={partnership.name}
-                        className="w-[590px] h-[300px] object-cover"
-                      />
-                      <img
-                        src={urlFor(partnership.logo).url()}
-                        alt={partnership.name}
-                        className="w-fit h-[80px] object-fit"
-                      />
-                    </div>
+                  <div className="flex flex-col gap-[8px] md:w-1/2 justify-start items-start">
+                    <img
+                      src={urlFor(partnership.coverPhoto).url()}
+                      alt={partnership.name}
+                      className="w-full h-[150px] md:h-[200px] lg:h-[300px] object-cover"
+                    />
+                    <img
+                      src={urlFor(partnership.logo).url()}
+                      alt={partnership.name}
+                      className="hidden md:block w-fit h-[40px] lg:h-[80px] object-contain"
+                    />
                   </div>
                 </div>
               ))}
 
-            <div className="flex flex-row gap-[32px] justify-between">
+            <div className="flex flex-col md:flex-row gap-[50px] md:gap-5 lg:gap-8 justify-between">
               {partnerships
                 ?.filter(
                   (partnership) =>
@@ -122,26 +87,28 @@ const SpotlightSection = () => {
                 .slice(2, 4)
                 .map((partnership) => (
                   <div
-                    className="flex flex-col w-1/2 gap-[32px]"
+                    className="flex flex-col-reverse md:flex-col md:w-1/2 gap-1 lg:gap-5"
                     key={partnership._id}
                   >
-                    <div className="flex flex-col gap-[8px]">
+                    <div className="flex flex-col-reverse md:flex-col gap-2">
                       <img
                         src={urlFor(partnership.coverPhoto).url()}
                         alt={partnership.name}
-                        className="w-[590px] h-[300px] object-cover"
+                        className="w-full md:h-[200px] lg:h-[300px] object-cover"
                       />
                       <img
                         src={urlFor(partnership.logo).url()}
                         alt={partnership.name}
-                        className="w-fit h-[80px] object-fit"
+                        className="w-fit hidden md:block h-[40px] lg:h-[80px] object-contain"
                       />
                     </div>
-                    <div className="w-[590px]">
-                      <h1 className="font-helvetica font-normal text-[16px] md:text-[32px]">
-                        {partnership.name}
-                      </h1>
-                      <h1 className="font-helvetica font-light text-[18px]">
+                    <div className="w-full flex flex-col gap-1">
+                      <img
+                        src={urlFor(partnership.logo).url()}
+                        alt={partnership.name}
+                        className="w-fit md:hidden h-[40px] lg:h-[80px] object-contain"
+                      />
+                      <h1 className="font-helvetica font-light text-[16px] lg:text-[18px] leading-[1.2]">
                         {partnership.description}
                       </h1>
                     </div>
