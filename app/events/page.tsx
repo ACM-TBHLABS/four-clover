7f2a9c10da1a6be5d708dc6754b1bee93324575e
saipepu <saipepu.mdy257@gmail.com>
2025-04-12 03:02:29 +0700

    Adjust max width of Workshop page layout for improved responsiveness

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -4,7 +4,7 @@ import WorkshopComponent from "./WorkshopPage"; // see below
 export default function WorkshopPageComponent() {
   return (
     <div className="w-full flex flex-col items-center justify-center overflow-x-hidden">
-      <div className="w-full max-w-screen md:max-w-[1600px]">
+      <div className="w-full max-w-screen md:max-w-[1400px]">
         <WorkshopComponent />
         <FAQSection /> {/* ✅ This stays as a Server Component */}
       </div>
