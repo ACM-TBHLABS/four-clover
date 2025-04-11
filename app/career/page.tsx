6aced4763b270bad14c1cedb5588cd3fb89097b9
saipepu <saipepu.mdy257@gmail.com>
2025-04-12 04:38:15 +0700

    Adjust padding of sections in CareerPage for improved layout

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -8,7 +8,7 @@ const CareerPage = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center overflow-x-hidden">
       <div className="w-full max-w-screen md:max-w-[1400px]">
-        <div className="flex flex-col gap-[50px] px-[24px] md:px-[150px]">
+        <div className="flex flex-col px-6 md:px-[50px] lg:px-[150px]">
           <HeroSection />
           <IntroSection />
           <JoinUsSection />
