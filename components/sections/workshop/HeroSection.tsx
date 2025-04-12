3ec4d9c48ee73d1f1a8dea09fec6aca6d5719911
LynnT-2003 <lnyinyi22@gmail.com>
2025-04-12 19:43:04 +0700

    Update: Deploy prod and update responsive UI for Events page.

--- components/sections/workshop/HeroSection.tsx
+++ components/sections/workshop/HeroSection.tsx
@@ -8,55 +8,57 @@ interface SectionProps {
 }
 
 const HeroSection: React.FC<SectionProps> = ({ event }) => {
-  if (!event) {
-    return (
-      <div className="w-full flex flex-col items-center justify-center overflow-x-hidden">
-        <div>Loading...</div>
-      </div>
-    );
-  }
   return (
     <div className="w-full ">
-      <TimerSection startDate={event.start_date} />
-      <div className="w-full h-[350px] md:h-auto md:aspect-video relative">
-        <img
-          src={urlFor(event.cover_image).url()}
-          className="w-full h-full object-cover"
-        />
-        <div className="absolute inset-0 bg-gradient-to-t from-white/40 to-transparent">
-          {/* Additional overlay at the bottom for text area */}
+      {event ? (
+        <>
+          <TimerSection startDate={event.start_date} />
+          <div className="w-full h-[350px] md:h-auto md:aspect-video relative">
+            <img
+              src={urlFor(event.cover_image).url()}
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute inset-0 bg-gradient-to-t from-white/40 to-transparent">
+              {/* Additional overlay at the bottom for text area */}
+            </div>
+            <div className="flex bottom-2 md:bottom-10 left-2 md:left-10 absolute flex-col gap-[8px] md:gap-[32px]">
+              <div className="flex flex-col gap-[8px] lg:gap-[16px] w-[80%]">
+                <h1 className="hidden md:block font-helvetica font-light text-[18px] leading-[100%]">
+                  {new Date(event.start_date).toLocaleString("en-US", {
+                    dateStyle: "long",
+                    timeStyle: "short",
+                  })}
+                  , {event.location}{" "}
+                </h1>
+                <h1 className="font-helvetica font-normal text-[24px] md:text-[56px] xl:text-[72px] leading-[100%]">
+                  {event.name}
+                </h1>
+                <h1 className="md:hidden font-helvetica font-light text-[14px] md:text-[18px] leading-[110%] md:leading-[100%] w-2/3">
+                  {new Date(event.start_date).toLocaleString("en-US", {
+                    dateStyle: "long",
+                    timeStyle: "short",
+                  })}
+                  , {event.location}{" "}
+                </h1>
+                <h1 className="hidden md:block font-helvetica font-light text-[20px] md:text-[24px] leading-[100%]">
+                  {event.tagline}
+                </h1>
+              </div>
+              <Link
+                href={`/events/${event.slug}`}
+                className="w-fit backdrop-blur-xl bg-white/50 border-black border-[1px] rounded-full font-helvetica font-light text-[16px] md:text-[32px] py-1 md:py-3 px-3 md:px-8"
+              >
+                Read More
+              </Link>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="w-full flex flex-col justify-center items-center overflow-x-hidden min-h-[80vh] gap-[25px]">
+          <div className="w-12 h-12 border-4 border-t-4 border-gray-200 rounded-full animate-spin border-t-green-700" />
+          <h1>Fetching events</h1>
         </div>
-        <div className="flex bottom-2 md:bottom-10 left-2 md:left-10 absolute flex-col gap-[8px] md:gap-[32px]">
-          {/* <div className="flex flex-col gap-[8px] md:gap-[16px]">
-            <h1 className="hidden md:block font-helvetica font-light text-[18px] leading-[100%]">
-              {new Date(event.start_date).toLocaleString("en-US", {
-                dateStyle: "long",
-                timeStyle: "short",
-              })}
-              , {event.location}{" "}
-            </h1>
-            <h1 className="font-helvetica font-normal text-[24px] md:text-[56px] lg:text-[72px] leading-[100%]">
-              {event.name}
-            </h1>
-            <h1 className="md:hidden font-helvetica font-light text-[14px] md:text-[18px] leading-[110%] md:leading-[100%] w-2/3">
-              {new Date(event.start_date).toLocaleString("en-US", {
-                dateStyle: "long",
-                timeStyle: "short",
-              })}
-              , {event.location}{" "}
-            </h1>
-            <h1 className="hidden md:block font-helvetica font-light text-[20px] md:text-[24px] leading-[100%]">
-              {event.tagline}
-            </h1>
-          </div> */}
-          <Link
-            href={`/events/${event.slug}`}
-            className="w-fit backdrop-blur-xl bg-white/50 border-black border-[1px] rounded-full font-helvetica font-light text-[16px] md:text-[32px] py-1 md:py-3 px-3 md:px-8"
-          >
-            Read More
-          </Link>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
