e0b891416c26d3cc4c4fa53abd098225285d3edf
saipepu <saipepu.mdy257@gmail.com>
2025-04-12 14:41:30 +0700

    Fix: Update heading line height for better readability and comment out Contact Us button

--- components/sections/productDetail/DescriptionSection.tsx
+++ components/sections/productDetail/DescriptionSection.tsx
@@ -18,18 +18,18 @@ const DescriptionSection: React.FC<SectionProps> = ({ title, desc }) => {
   return (
     <div className="py-[100px] flex gap-[10px] relative">
       <div className="w-full flex flex-col gap-[12px] relative">
-        <h1 className="font-helvetica font-bold text-[32px] leading-[36.8px]">
+        <h1 className="font-helvetica font-bold text-[32px] leading-[1.1]">
           {title}
         </h1>
         <div className="font-helvetica">
           <PortableText value={desc} components={portableTextComponents}/>
         </div>
-        <button
+        {/* <button
           className="hover:bg-black mt-10 hover:text-white transition-all duration-300 hover:scale-105 ease-in-out w-[280px] py-[16px] px-[24px] border-solid border-black border-[0.5px] rounded-[5px] text-center font-helvetica font-normal text-[24px] leading-[27.6px]"
           onClick={() => router.push("/contact")}
         >
           Contact Us
-        </button>
+        </button> */}
       </div>
       <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[50%] border-b-[1px] border-[#666666]"></div>
     </div>
